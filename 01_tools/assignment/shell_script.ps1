1785c78c3427830403d8dc6f3ff9178a79954765
Kayla Man <moonyuet2008@hotmail.cocm>
2022-07-16 19:43:54 +0800

    ADD Script to 01_TOOLS and a python script of texture importer in maya

---
+++ 01_tools/assignment/shell_script.ps1
@@ -0,0 +1,20 @@
+
+    
+mkdir shell_test
+
+
+    
+cd shell_test
+touch test_print.py
+cat > test_print.py << "END_SCRIPT"
+END_SCRIPT
+ 
+    
+mv test_print.py new_test_print.py
+    
+ls shell_test
+  
+python new_test_print.py
+    
+rm -r shell_test
+
---
+++ 01_tools/assignment/start_nuke.bat
@@ -0,0 +1,9 @@
+@echo off
+cd "C:/Program Files/Nuke13.2v1"
+set "PROJECT_ROOT = "/Double_Exposure_Nuke"
+set "NUKE_PATH = %PROJECT_ROOT%"
+set "NUKE_INIT_PATH = %PROJECT_ROOT%"
+
+start Nuke13.2.exe
+
+
