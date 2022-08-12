16e302a7609c6c5a55320be082abdd45829f4db3
Kayla Man <moonyuet2008@hotmail.cocm>
2022-08-12 20:23:26 +0800

    UPDATE 03_advanced Style and 04_ui concept

---
+++ 03_advanced/kman_assignment_03/assignment_advanced.py
@@ -0,0 +1,69 @@
+# ADVANCED **************************************************************************
+# content = assignment (Python Advanced)
+#
+# deliver = Upload the files to your git repository
+#           and share it in the assignment submission form.
+#
+# date    = 2022-01-07
+# email   = contact@alexanderrichtertd.com
+#*******************************************************************************
+"""
+
+Advanced Python is about a higher and more structural level of Python.
+Learning new functions, constructs and a better way of collaboration.
+
+
+
+#*******************************************************************************
+# 01. CUBE CLASS
+#*******************************************************************************
+Create a cube class in cube.py
+
+
+
+
+
+#*******************************************************************************
+# 02. UPDATE CLASS
+#*******************************************************************************
+a) Update one of your modules using classes.
+   Make sure to use one of the benefits of classes (else it's pointless).
+   Write a comment in the docString of the class why a class would improve this module.
+
+
+b) Update one of your modules using class parenting.
+   Write a comment in the docString of the class why parenting would improve this module.
+
+   ALTERNATIVE: Should you have a hard time finding a good example in your code,
+   create a hypothetical situation where parenting would make sense.
+
+
+NOTE: a) & b) could be the same task.
+
+
+
+
+
+
+
+#*******************************************************************************
+# 04. CONFIGURATION FILES
+#*******************************************************************************
+
+    • Update one of your modules using configuration files.
+    • Write a comment in the call of the configuration file
+      why a configuration file would improve this module.
+
+
+
+
+
+
+#*******************************************************************************
+# 05. DECORATOR
+#*******************************************************************************
+Create a decorator in decorator.py
+
+
+
+"""
